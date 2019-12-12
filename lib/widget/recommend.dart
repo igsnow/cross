409af6a72a1917bd1329a482b2257e95bc2457e0
igsnow <zzy716810918@gmail.com>
2019-12-12 15:49:12 +0800

    好货推荐标题设置

--- lib/widget/recommend.dart
+++ lib/widget/recommend.dart
@@ -3,23 +3,27 @@ import 'package:flutter/material.dart';
 class Recommend extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Container(
+      margin: EdgeInsets.fromLTRB(8, 0, 8, 0),
       decoration: BoxDecoration(
         color: Colors.red,
-        borderRadius: BorderRadius.all(Radius.circular(6)),
+        borderRadius: BorderRadius.all(Radius.circular(2)),
       ),
       child: Column(
         children: <Widget>[
-          Align(
-            alignment: Alignment.topLeft,
+          Container(
+            width: screenWidth,
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
               child: Text(
                 '好货推荐',
                 textAlign: TextAlign.left,
                 style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.w700,
+                  height: 2.8,
                 ),
               ),
             ),
@@ -27,7 +31,7 @@ class Recommend extends StatelessWidget {
           Text(
             'zzy',
             style: TextStyle(
-              height: 10,
+              height: 3,
             ),
           )
         ],
